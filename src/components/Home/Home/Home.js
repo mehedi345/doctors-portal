dacb75287bad087ed73af0f807f0ad20d9f1be7c
mehedi345 <mehedihasan5101220@gmail.com>
2020-10-29 11:00:55 -0700

    update home page

--- src/components/Home/Home/Home.js
+++ src/components/Home/Home/Home.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import Blog from '../Blog/Blog';
+import Contact from '../Contact/Contact';
+import Doctors from '../Doctors/Doctors';
+import FeaturedService from '../FeaturedService.js/FeaturedService';
+import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
+import MakeAppoinment from '../MakeAppoinment/MakeAppoinment';
 import Services from '../Services/Services';
-
+import Testimonials from '../Testimonials/Testimonials';
 const Home = () => {
     return (
         <div>
             <Header/>
             <Services/>
+            <FeaturedService/>
+            <MakeAppoinment/>
+            <Testimonials/>
+            <Blog/>
+            <Doctors/>
+            <Contact/>
+            <Footer/>
         </div>
     );
 };
